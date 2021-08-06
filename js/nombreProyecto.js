2c87536fe7c188c04be3a7ba264730b447c9f7ae
guadaMongeBarale <guadamongebarale@gmail.com>
2021-08-05 23:37:54 -0300

    Leer comentario

---
+++ js/nombreProyecto.js
@@ -0,0 +1,33 @@
+const nombreProyecto = "Dreamteam";
+
+document.getElementById("npro").innerHTML = nombreProyecto;
+document.getElementById("np2").innerHTML = nombreProyecto;
+document.getElementById("np3").innerHTML = nombreProyecto;
+document.getElementById("np4").innerHTML = nombreProyecto;
+document.getElementById("np5").innerHTML = nombreProyecto;
+document.getElementById("np6").innerHTML = nombreProyecto;
+document.getElementById("np7").innerHTML = nombreProyecto;
+document.getElementById("np8").innerHTML = nombreProyecto;
+document.getElementById("np9").innerHTML = nombreProyecto;
+document.getElementById("np10").innerHTML = nombreProyecto;
+document.getElementById("np11").innerHTML = nombreProyecto;
+document.getElementById("np12").innerHTML = nombreProyecto;
+document.getElementById("np13").innerHTML = nombreProyecto;
+document.getElementById("np14").innerHTML = nombreProyecto;
+document.getElementById("np15").innerHTML = nombreProyecto;
+document.getElementById("np16").innerHTML = nombreProyecto;
+document.getElementById("np17").innerHTML = nombreProyecto;
+document.getElementById("np18").innerHTML = nombreProyecto;
+document.getElementById("np19").innerHTML = nombreProyecto;
+document.getElementById("np20").innerHTML = nombreProyecto;
+
+
+
+
+
+
+
+
+
+
+
