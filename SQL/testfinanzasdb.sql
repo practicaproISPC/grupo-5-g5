434b817f0a90f9cc761ebf7fb85a3bd1567a6f09
leiva7 <leivapaula7@gmail.com>
2021-08-31 22:57:39 -0300

    se realizan modificaciones en la tabla visitantes de la base de datos, y en la conexión a la base de datos para tolerancia de ñ y ascentos

--- SQL/testfinanzasdb.sql
+++ SQL/testfinanzasdb.sql
@@ -1,20 +1,22 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.1
--- http://www.phpmyadmin.net
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
 --
--- Servidor: localhost
--- Tiempo de generación: 31-08-2021 a las 15:47:57
--- Versión del servidor: 5.5.24-log
--- Versión de PHP: 5.4.3
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 01-09-2021 a las 01:55:13
+-- Versión del servidor: 10.4.10-MariaDB
+-- Versión de PHP: 7.3.12
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de datos: `testfinanzasdb`
@@ -26,13 +28,14 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `usuario_log`
 --
 
+DROP TABLE IF EXISTS `usuario_log`;
 CREATE TABLE IF NOT EXISTS `usuario_log` (
   `id_user` int(11) NOT NULL AUTO_INCREMENT,
   `USUARIO` varchar(50) NOT NULL,
   `EMAIL` varchar(100) NOT NULL,
   `CONTRA` varchar(50) NOT NULL,
   PRIMARY KEY (`id_user`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `usuario_log`
@@ -48,26 +51,29 @@ INSERT INTO `usuario_log` (`id_user`, `USUARIO`, `EMAIL`, `CONTRA`) VALUES
 -- Estructura de tabla para la tabla `visitantes`
 --
 
+DROP TABLE IF EXISTS `visitantes`;
 CREATE TABLE IF NOT EXISTS `visitantes` (
   `id_visitante` int(10) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(45) NOT NULL,
-  `apellido` varchar(45) NOT NULL,
-  `email` varchar(100) NOT NULL,
+  `nombre` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
+  `apellido` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
+  `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
   `telefono` bigint(20) NOT NULL,
-  `domicilio` varchar(100) NOT NULL,
-  `consulta` varchar(255) NOT NULL,
+  `domicilio` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
+  `consulta` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
   PRIMARY KEY (`id_visitante`),
   KEY `id_visitante` (`id_visitante`),
   KEY `nombre` (`nombre`,`apellido`,`email`,`telefono`,`domicilio`,`consulta`),
   KEY `id_visitante_2` (`id_visitante`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `visitantes`
 --
 
 INSERT INTO `visitantes` (`id_visitante`, `nombre`, `apellido`, `email`, `telefono`, `domicilio`, `consulta`) VALUES
-(12, 'guada', 'dsgadg', 'guadamongebarale@gmail.com', 3644559255, '', '');
+(13, 'Juan', 'Rodriguez', 'jr@gmail.com', 3519998877, 'lugar 875', 'Muchas más consultas'),
+(14, 'Laura', 'Diaz', 'laudiaz@gmail.com', 3517774455, 'Chacabuco 974', 'Tengo una consulta que tiene muchos ascentos como estos á é í ó ú y otras tantas ñ eñes ñá ñé ñí ñó ñú'),
+(15, 'Matilda', 'Fernandez', 'matif@hotmail.com', 34799784412, 'Los Tulipanes 765', 'Quiero saber más sobre mis finanzas personales. Quiero mejorar la inversión de lo que gano con mi desempeño laboral.');
 
 -- --------------------------------------------------------
 
@@ -75,6 +81,7 @@ INSERT INTO `visitantes` (`id_visitante`, `nombre`, `apellido`, `email`, `telefo
 -- Estructura de tabla para la tabla `visitas`
 --
 
+DROP TABLE IF EXISTS `visitas`;
 CREATE TABLE IF NOT EXISTS `visitas` (
   `id_visita` int(10) NOT NULL AUTO_INCREMENT,
   `id_visitante` int(10) NOT NULL,
@@ -86,7 +93,7 @@ CREATE TABLE IF NOT EXISTS `visitas` (
   KEY `id_visitante_2` (`id_visitante`),
   KEY `id_visitante_3` (`id_visitante`),
   KEY `id_visita` (`id_visita`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Restricciones para tablas volcadas
@@ -97,6 +104,7 @@ CREATE TABLE IF NOT EXISTS `visitas` (
 --
 ALTER TABLE `visitantes`
   ADD CONSTRAINT `visitantes_ibfk_1` FOREIGN KEY (`id_visitante`) REFERENCES `visitantes` (`id_visitante`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
